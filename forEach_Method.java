06bbccf68d34d0edccff4b77f26ac31bbee1566c
Anix00011 <31801904+anix00011@users.noreply.github.com>
2018-07-27 22:42:05 +0530

    Update forEach_Method.java

--- forEach_Method.java
+++ forEach_Method.java
@@ -30,3 +30,21 @@ public class ForEachExample {
         gamesList.forEach(System.out::println);  
     }  
 }  
+//***************************************************************************************************************************************
+//using stream
+import java.util.ArrayList;
+import java.util.List;
+public class test {
+    public static void main(String[] args) {
+        List<String> gamesList = new ArrayList<String>();
+        gamesList.add("Football");
+        gamesList.add("Cricket");
+        gamesList.add("Chess");
+        gamesList.add("Hocky");
+        System.out.println("------------Iterating by passing lambda expression---------------");
+        gamesList.stream().forEach(games -> System.out.println(games));
+        System.out.println("------------Iterating by passing method reference---------------");
+        gamesList.stream().forEach(System.out::println);
+    }
+
+}
