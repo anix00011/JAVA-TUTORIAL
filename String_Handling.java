e68a8b79c6cb24b9bd79db7e961518291a7689a2
Anix00011 <31801904+anix00011@users.noreply.github.com>
2018-07-28 16:01:12 +0530

    Update String_Handling.java

--- String_Handling.java
+++ String_Handling.java
@@ -13,6 +13,17 @@ java
 strings
 example
 //***************************************************************************************************************************************
+public class StringToCharArrayExample{  
+public static void main(String args[]){  
+String s1="hello";  
+char[] ch=s1.toCharArray();  
+for(int i=0;i<ch.length;i++){  
+System.out.print(ch[i]);  
+}  
+}}  
+Output:
+hello
+//***************************************************************************************************************************************
 public class CharAtExample{  
 public static void main(String args[]){  
 String name="javatpoint";  
